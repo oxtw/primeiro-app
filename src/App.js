39977573f9195835ec1fe0d98deeea983ead7e32
miguel <weigertmiguel@hotmail.com>
2024-11-18 16:41:57 -0300

    manipulando-formularios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-import Nome from './components/Nome';
 import { useState } from 'react';
 
 function App(){
-  const [aluno, setAluno] = useState('Miguel');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
 
-  function handleChangeName(nome){
-    setAluno(nome)
+  const [user, setUser] = useState({});
+
+  //função para registar o formulário
+  function handleRegister(e){
+    //função para não recarregar a pagina após confirmar o envio do formulário
+    e.preventDefault();
+
+    alert('Usuario registrado com sucesso!')
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
   }
+
 
   return(
     <div>
-      <h1>Componente App</h1><br/>
-      <h2>Olá: {aluno}</h2>
-      <button onClick={ () => handleChangeName('Miguel Weigert') }>
-        Mudar Nome
-      </button>
-      {/* <Nome aluno="Lucas" idade={30}/> */}
+      <h1>Cadastrando Usuário</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome:</label><br/>
+        <input 
+        placeholder='Digite seu nome'
+        value={nome}
+        onChange={ (e) => setNome(e.target.value) }
+        ></input><br/>
+        
+        <label>Email:</label><br/>
+        <input 
+        placeholder='Digite seu email'
+        value={email}
+        onChange={ (e) => setEmail(e.target.value)}
+        ></input><br/>
+        
+        <label>Idade:</label><br/>
+        <input 
+        placeholder='Digite sua idade'
+        value={idade}
+        onChange={ (e) => setIdade(e.target.value)}
+        ></input><br/>
+
+        <button type='submit'>Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <div>
+
+       <span>Bem vindo: {user.nome}</span><br/>
+       <span>Email: {user.email}</span><br/>
+       <span>Idade: {user.idade}</span><br/>
+
+      </div>
     </div>
   );
 }
