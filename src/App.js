9875b602f2197d4bfbcad1661886b56fce7a1b54
miguel <weigertmiguel@hotmail.com>
2024-11-18 16:01:12 -0300

    entendendo-props-e-componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Nome from './components/Nome';
 
-function App() {
-  return (
+
+function App(){
+  return(
     <div>
-      <h1>Bem vindo ao meu Projeto</h1>
+      <h1>Componente App</h1>
+      <Nome aluno="Lucas" idade={30}/>
+      <br/>
+      <Nome aluno="Jose Silva" idade={12}/>
     </div>
   );
 }
