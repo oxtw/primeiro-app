20c42bd30242cdd77d7f1ef92a5db7a23ed71fb4
miguel <weigertmiguel@hotmail.com>
2024-11-18 16:13:13 -0300

    trabalhando-com-use-state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Nome from './components/Nome';
-
+import { useState } from 'react';
 
 function App(){
+  const [aluno, setAluno] = useState('Miguel');
+
+  function handleChangeName(nome){
+    setAluno(nome)
+  }
+
   return(
     <div>
-      <h1>Componente App</h1>
-      <Nome aluno="Lucas" idade={30}/>
-      <br/>
-      <Nome aluno="Jose Silva" idade={12}/>
+      <h1>Componente App</h1><br/>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={ () => handleChangeName('Miguel Weigert') }>
+        Mudar Nome
+      </button>
+      {/* <Nome aluno="Lucas" idade={30}/> */}
     </div>
   );
 }
